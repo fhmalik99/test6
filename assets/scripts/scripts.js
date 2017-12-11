cbbbf52349b443d7d4610b39d6223db81c689c75
Paul Jacobson <pljcbsn@gmail.com>
2017-12-11 21:32:03 +0200

    Adding another page and some Disney stuff

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,7 +3,7 @@ const btn = document.querySelector('.btn-container');
 
 async function birthdayGif() {
   try {
-    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=GNeXyRWAB7D9CGK8qsM9X214SPFpcXKw&tag=ponies&rating=G'); // This sends the request
+    const response = await fetch('https://api.giphy.com/v1/gifs/random?api_key=GNeXyRWAB7D9CGK8qsM9X214SPFpcXKw&tag=disneyanimation&rating=G'); // This sends the request
     if (response.ok) { // This handles a successful response
       let jsonResponse = await response.json();
       gifSpace.innerHTML = `<img src="${jsonResponse.data.image_url}">`;
